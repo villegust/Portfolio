fa03b2b36bb58f4343ac9abb01617a7abac7f0a6
villegust <ville.gust@gmail.com>
2023-05-26 16:52:11 +0200

    Updates

---
+++ precache-manifest.b9663dd0a0b4c2b3dcb9affdf43485c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50043bbb3bd731c758d6",
+    "url": "/portfolio/static/css/main.254d105c.chunk.css"
+  },
+  {
+    "revision": "50043bbb3bd731c758d6",
+    "url": "/portfolio/static/js/main.50043bbb.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "46fda2b447f5dda00b8c56f8f1da3366",
+    "url": "/portfolio/index.html"
+  }
+];
